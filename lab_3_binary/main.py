8e132fd265dd676cbe77d89760ad6f664d4c3b79
Dvuglaf <aizeensan@gmail.com>
2022-10-13 22:19:52 +0400

    add wrong classification, show all needed pictures

--- lab_3_binary/main.py
+++ lab_3_binary/main.py
@@ -2,6 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage.io import show, imshow
 from scipy.special import erf
+import warnings
+
+
+warnings.filterwarnings("ignore")
 
 
 def phi_laplace(x) -> float:
@@ -24,11 +28,12 @@ def get_probability(dataset: np.array) -> float:
     return ones_count / dataset.shape[0]
 
 
-def get_lambda(x: np.array, prob_1: np.array, prob_2: np.array) -> np.array:
-    w = np.log(prob_1 * (1 - prob_2) /
-               ((1 - prob_1) * prob_2))
+def get_w(prob_1: np.array, prob_2: np.array) -> np.array:
+    return np.log(prob_1 * (1 - prob_2) / ((1 - prob_1) * prob_2))
 
-    return np.sum(x * w)
+
+def get_lambda(x: np.array, prob_1: np.array, prob_2: np.array) -> np.array:
+    return np.sum(x * get_w(prob_1, prob_2))
 
 
 def get_threshold(
@@ -72,13 +77,17 @@ def get_errors(
 def classification_error(
     dataset: np.array, aprior_1: float, aprior_2: float,
     prob_1: float, prob_2: float, N: int
-) -> float:
+):
     errors = 0  # показывает число неверно определенных элементов
+    indices_wrong_classified = []
 
-    for x in dataset:
-        errors += bayes_classificator(x, aprior_1, aprior_2, prob_1, prob_2)
+    for i in range(N):
+        result = bayes_classificator(dataset[i], aprior_1, aprior_2, prob_1, prob_2)
+        errors += result
+        if result == 1:
+            indices_wrong_classified.append(i)
 
-    return errors / N  # ошибка первого рода
+    return errors / N, np.array(indices_wrong_classified)
 
 
 # Подсчет относительной погрешности
@@ -87,6 +96,12 @@ def get_e(p: float, N: int) -> float:
         return np.sqrt((1 - p) / (N * p))
     except ZeroDivisionError:
         return 1.
+
+
+def get_right_classified(dataset: np.array, ind_wrong_classified: np.array):
+    for i in range(dataset.shape[0]):
+        if i not in ind_wrong_classified:
+            return dataset[i].reshape((9, 9))
 
 
 def main():
@@ -122,24 +137,62 @@ def main():
 
     errors = get_errors(aprior_u, aprior_z, prob_u, prob_z)
 
-    experimental_errors = [classification_error(dataset_u, aprior_u, aprior_z, prob_u, prob_z, N),
-                           classification_error(dataset_z, aprior_z, aprior_u, prob_z, prob_u, N)]
+    exp_error_u, wrong_classified_u = classification_error(dataset_u, aprior_u, aprior_z, prob_u, prob_z, N)
+    exp_error_z, wrong_classified_z = classification_error(dataset_z, aprior_z, aprior_u, prob_z, prob_u, N)
 
-    print(f"{errors[0][0]}, {errors[0][1]}")
-    print(f"{experimental_errors[0]}, {experimental_errors[1]}")
+    print(f"Аналитические ошибки классификации: {errors[0][0]}, {errors[0][1]}")
+    print(f"Экспериментальные ошибки классификации: {exp_error_u}, {exp_error_z}")
+
+    fig = plt.figure()
+    sp = fig.add_subplot(2, 2, 1)
+    sp.set_title("Класс U")
+    imshow(1 - symbol_u, cmap='gray')
+    sp = fig.add_subplot(2, 2, 2)
+    sp.set_title("Класс Z")
+    imshow(1 - symbol_z, cmap='gray')
+    sp = fig.add_subplot(2, 2, 3)
+    sp.set_title("Верно классифицированный в класс U")
+    imshow(1 - get_right_classified(dataset_u, wrong_classified_u), cmap='gray')
+    sp = fig.add_subplot(2, 2, 4)
+    sp.set_title("Верно классифицированный в класс Z")
+    imshow(1 - get_right_classified(dataset_z, wrong_classified_z), cmap='gray')
 
     plt.figure()
-    plt.plot(range(0, 81), prob_u, color='pink', label='probability U')
-    plt.plot(range(0, 81), prob_z, color='blue', label='probability Z')
+    plt.title("Распределение вероятностей (X=1/OMEGA)")
+    plt.plot(range(0, 81), prob_u, color='pink', label='(X=1/U)')
+    plt.plot(range(0, 81), prob_z, color='blue', label='(X=1/Z)')
     plt.legend()
 
     fig = plt.figure()
     fig.add_subplot(1, 2, 1)
-    plt.title("Распределение вероятностей (X = 1/U)")
+    plt.title("Распределение вероятностей (X=1/U)")
     imshow(prob_u.reshape((9, 9)), cmap='gray')
     fig.add_subplot(1, 2, 2)
-    plt.title("Распределение вероятностей (X = 1/Z)")
+    plt.title("Распределение вероятностей (X=1/Z)")
     imshow(prob_z.reshape((9, 9)), cmap='gray')
+
+    if wrong_classified_u.size != 0:
+        plt.figure()
+        plt.title("Неверно классифицированный объект класса U")
+        imshow(1 - dataset_u[wrong_classified_u[0]].reshape((9, 9)), cmap='gray')
+
+    if wrong_classified_z.size != 0:
+        plt.figure()
+        plt.title("Неверно классифицированный объект класса Z")
+        imshow(1 - dataset_u[wrong_classified_z[0]].reshape((9, 9)), cmap='gray')
+
+    w_u_z = get_w(prob_u, prob_z)
+    w_z_u = get_w(prob_z, prob_u)
+
+    fig = plt.figure()
+    sp = fig.add_subplot(1, 2, 1)
+    sp.set_title("Компоненты вектора w_u_z")
+    imshow(w_u_z.reshape((9, 9)), cmap='gray')
+    sp = fig.add_subplot(1, 2, 2)
+    sp.set_title("Компоненты вектора w_z_u")
+    imshow(w_z_u.reshape((9, 9)), cmap='gray')
+
     show()
+
 
 main()
