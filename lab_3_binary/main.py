d07c44f4e239fe9bdeb43cac6114c704d2e08b44
Dvuglaf <aizeensan@gmail.com>
2022-10-20 00:02:03 +0400

    add printing number wrong classified, fixed errors in return type, add comments

--- lab_3_binary/main.py
+++ lab_3_binary/main.py
@@ -12,6 +12,8 @@ def phi_laplace(x) -> float:
     return (1 + erf(x / np.sqrt(2))) / 2
 
 
+# Генерируется случайный вектор N со значениями [0., 1.].
+# Если значение вектора меньше вероятности p, то соответствующее значение представителя класса инвертируется.
 def generate_dataset(symbol: np.array, N: int, p: float) -> np.array:
     dataset = []
 
@@ -23,74 +25,81 @@ def generate_dataset(symbol: np.array, N: int, p: float) -> np.array:
     return np.array(dataset)
 
 
-def get_probability(dataset: np.array) -> float:
+# Подсчет распределения вероятностей.
+# Считается количество единиц по всем элементам выборки на соответствующих местах и делится на объем выборки.
+def get_probability(dataset: np.array) -> np.array:
     ones_count = np.sum(dataset, axis=0)
     return ones_count / dataset.shape[0]
 
 
-def get_w(prob_1: np.array, prob_2: np.array) -> np.array:
-    return np.log(prob_1 * (1 - prob_2) / ((1 - prob_1) * prob_2))
+def get_w(prob_0: np.array, prob_1: np.array) -> np.array:
+    return np.log(prob_0 * (1 - prob_1) / ((1 - prob_0) * prob_1))
 
 
-def get_lambda(x: np.array, prob_1: np.array, prob_2: np.array) -> np.array:
-    return np.sum(x * get_w(prob_1, prob_2))
+# Отношение правдоподобия.
+def get_lambda(x: np.array, prob_0: np.array, prob_1: np.array) -> np.array:
+    return np.sum(x * get_w(prob_0, prob_1))
 
 
+# Пороговое значение для отношения правдоподобия (ln лямбды маленькой).
 def get_threshold(
-    aprior_1: float, aprior_2: float, prob_1: float, prob_2: float
+    aprior_0: float, aprior_1: float, prob_0: float, prob_1: float
 ) -> np.array:
-    return np.log(aprior_2 / aprior_1) + \
-        np.sum(np.log((1 - prob_2) / (1 - prob_1)))
+    return np.log(aprior_1 / aprior_0) + np.sum(np.log((1 - prob_1) / (1 - prob_0)))
 
 
+# Байесовский классификатор по отношению правдоподобия.
 def bayes_classificator(
-    x: np.array, aprior_1: float, aprior_2: float, prob_1: float, prob_2: float
+    x: np.array, aprior_0: float, aprior_1: float, prob_0: float, prob_1: float
 ) -> int:
-    _Lambda = get_lambda(x, prob_1, prob_2)
-    _lambda = get_threshold(aprior_1, aprior_2, prob_1, prob_2)
+    _Lambda = get_lambda(x, prob_0, prob_1)
+    _lambda = get_threshold(aprior_0, aprior_1, prob_0, prob_1)
 
     return 0 if _Lambda >= _lambda else 1
 
 
+# Аналитические вероятности ошибочной классификации (p_0 и p_1).
+# @return список вероятностей при выполнении ЦПТ и не выполнении ЦПТ (верхние границы).
 def get_errors(
-    aprior_1: float, aprior_2: float, prob_1: float, prob_2: float
+    aprior_0: float, aprior_1: float, prob_0: float, prob_1: float
 ) -> list:
-    base = np.log(prob_2 * (1 - prob_1) / ((1 - prob_2) * prob_1))
+    w_1_0 = np.log(prob_1 * (1 - prob_0) / ((1 - prob_1) * prob_0))
 
-    m_1 = np.sum(base * prob_1)
-    m_2 = np.sum(base * prob_2)
+    m_0 = np.sum(w_1_0 * prob_0)
+    m_1 = np.sum(w_1_0 * prob_1)
 
-    sigma_1 = np.sqrt(np.sum((base ** 2) * prob_1 * (1 - prob_1)))
-    sigma_2 = np.sqrt(np.sum((base ** 2) * prob_2 * (1 - prob_2)))
+    sigma_0 = np.sqrt(np.sum((w_1_0 ** 2) * prob_0 * (1 - prob_0)))
+    sigma_1 = np.sqrt(np.sum((w_1_0 ** 2) * prob_1 * (1 - prob_1)))
 
-    _lambda = np.log(aprior_1 / aprior_2)
+    _lambda = np.log(aprior_0 / aprior_1)
 
-    errors = [1 - phi_laplace((_lambda - m_1) / sigma_1),
-              phi_laplace((_lambda - m_2) / sigma_2)]
-    upper_bounds = [sigma_1 ** 2 / (((m_1) - _lambda) ** 2),
-                    sigma_2 ** 2 / (((m_2) - _lambda) ** 2)]
+    errors = [1 - phi_laplace((_lambda - m_0) / sigma_0),
+              phi_laplace((_lambda - m_1) / sigma_1)]
+    upper_bounds = [sigma_0 ** 2 / ((m_0 - _lambda) ** 2),
+                    sigma_1 ** 2 / ((m_1 - _lambda) ** 2)]
 
-    return errors, upper_bounds
+    return [errors, upper_bounds]
 
 
-# Подсчет ошибки
+# Экспериментальные ошибки классификации (где 1, там ошибочная классификация).
+# @return ошибки классификации, индексы неверно классифицированных объектов.
 def classification_error(
-    dataset: np.array, aprior_1: float, aprior_2: float,
-    prob_1: float, prob_2: float, N: int
-):
-    errors = 0  # показывает число неверно определенных элементов
+    dataset: np.array, aprior_0: float, aprior_1: float,
+    prob_0: float, prob_1: float, N: int
+) -> tuple[float, np.array]:
+    num_errors = 0
     indices_wrong_classified = []
 
     for i in range(N):
-        result = bayes_classificator(dataset[i], aprior_1, aprior_2, prob_1, prob_2)
-        errors += result
+        result = bayes_classificator(dataset[i], aprior_0, aprior_1, prob_0, prob_1)
+        num_errors += result
         if result == 1:
             indices_wrong_classified.append(i)
 
-    return errors / N, np.array(indices_wrong_classified)
+    return num_errors / N, np.array(indices_wrong_classified)
 
 
-# Подсчет относительной погрешности
+# Подсчет относительной погрешности.
 def get_e(p: float, N: int) -> float:
     try:
         return np.sqrt((1 - p) / (N * p))
@@ -98,13 +107,15 @@ def get_e(p: float, N: int) -> float:
         return 1.
 
 
-def get_right_classified(dataset: np.array, ind_wrong_classified: np.array):
+# Получение верно распознанных объектов.
+def get_right_classified(dataset: np.array, ind_wrong_classified: np.array) -> np.array:
     for i in range(dataset.shape[0]):
         if i not in ind_wrong_classified:
             return dataset[i].reshape((9, 9))
 
 
 def main():
+    # Представители класса
     symbol_u = np.array([[0, 1, 0, 0, 0, 0, 0, 1, 0],
                          [0, 1, 0, 0, 0, 0, 0, 1, 0],
                          [0, 1, 0, 0, 0, 0, 0, 1, 0],
@@ -124,16 +135,34 @@ def main():
                          [0, 0, 1, 0, 0, 0, 0, 0, 0],
                          [0, 1, 0, 0, 0, 0, 0, 0, 0],
                          [0, 1, 1, 1, 1, 1, 1, 1, 0]])
+    N = 200  # объем выборки
+    aprior_u = aprior_z = 0.5  # априорные вероятности
+    p = 0.3  # вероятность инвертировать бит
 
-    N = 200
+    dataset_u = generate_dataset(symbol_u, N, p)
+    dataset_z = generate_dataset(symbol_z, N, p)
 
-    dataset_u = generate_dataset(symbol_u, N, 0.3)
-    dataset_z = generate_dataset(symbol_z, N, 0.3)
+    analytic_prob_u = np.array([[0.3, 0.7, 0.3, 0.3, 0.3, 0.3, 0.3, 0.7, 0.3],
+                                [0.3, 0.7, 0.3, 0.3, 0.3, 0.3, 0.3, 0.7, 0.3],
+                                [0.3, 0.7, 0.3, 0.3, 0.3, 0.3, 0.3, 0.7, 0.3],
+                                [0.3, 0.7, 0.3, 0.3, 0.3, 0.3, 0.3, 0.7, 0.3],
+                                [0.3, 0.7, 0.3, 0.3, 0.3, 0.3, 0.3, 0.7, 0.3],
+                                [0.3, 0.7, 0.3, 0.3, 0.3, 0.3, 0.3, 0.7, 0.3],
+                                [0.3, 0.7, 0.3, 0.3, 0.3, 0.3, 0.3, 0.7, 0.3],
+                                [0.3, 0.3, 0.7, 0.3, 0.3, 0.3, 0.7, 0.3, 0.3],
+                                [0.3, 0.3, 0.3, 0.7, 0.7, 0.7, 0.3, 0.3, 0.3]])
+    analytic_prob_z = np.array([[0.3, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.3],
+                                [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.7, 0.3],
+                                [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.7, 0.3, 0.3],
+                                [0.3, 0.3, 0.3, 0.3, 0.3, 0.7, 0.3, 0.3, 0.3],
+                                [0.3, 0.3, 0.3, 0.3, 0.7, 0.3, 0.3, 0.3, 0.3],
+                                [0.3, 0.3, 0.3, 0.7, 0.3, 0.3, 0.3, 0.3, 0.3],
+                                [0.3, 0.3, 0.7, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3],
+                                [0.3, 0.7, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3],
+                                [0.3, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.3]])
 
-    aprior_u = aprior_z = 0.5
-
-    prob_u = get_probability(dataset_u)
-    prob_z = get_probability(dataset_z)
+    prob_u = get_probability(dataset_u)  # закон распределения (x = 1/u)
+    prob_z = get_probability(dataset_z)  # закон распределения (x = 1/z)
 
     errors = get_errors(aprior_u, aprior_z, prob_u, prob_z)
 
@@ -142,6 +171,8 @@ def main():
 
     print(f"Аналитические ошибки классификации: {errors[0][0]}, {errors[0][1]}")
     print(f"Экспериментальные ошибки классификации: {exp_error_u}, {exp_error_z}")
+    print(f"Число неверно классифицированных объектов класса U: {len(wrong_classified_u)}")
+    print(f"Число неверно классифицированных объектов класса Z: {len(wrong_classified_z)}")
 
     fig = plt.figure()
     sp = fig.add_subplot(2, 2, 1)
@@ -171,14 +202,14 @@ def main():
     plt.title("Распределение вероятностей (X=1/Z)")
     imshow(prob_z.reshape((9, 9)), cmap='gray')
 
+    fig = plt.figure()
+    fig.add_subplot(1, 2, 1)
+    plt.title("Неверно классифицированный объект класса U")
     if wrong_classified_u.size != 0:
-        plt.figure()
-        plt.title("Неверно классифицированный объект класса U")
         imshow(1 - dataset_u[wrong_classified_u[0]].reshape((9, 9)), cmap='gray')
-
+    fig.add_subplot(1, 2, 2)
+    plt.title("Неверно классифицированный объект класса Z")
     if wrong_classified_z.size != 0:
-        plt.figure()
-        plt.title("Неверно классифицированный объект класса Z")
         imshow(1 - dataset_u[wrong_classified_z[0]].reshape((9, 9)), cmap='gray')
 
     w_u_z = get_w(prob_u, prob_z)
