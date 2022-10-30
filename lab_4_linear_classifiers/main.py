65da0d6a976962c3ce6eada9d400fe09b277742f
meshcheryakov-a <meshcheryakov-a@internet.ru>
2022-10-31 00:22:06 +0400

    linear border and some refactoring

--- lab_4_linear_classifiers/main.py
+++ lab_4_linear_classifiers/main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from skimage.io import show
 from lab_2_classifiers.main import bayes_classificator, bayes_discriminant, bayes_border_fixed
 
 
@@ -15,17 +17,27 @@ def get_risk(aprior0: float, aprior1: float, p0: float, p1: float) -> float:
     return aprior0 * p0 + aprior1 * p1
 
 
-def fisher_discriminant(x: np.array, m0: np.array, m1: np.array, b0: np.array, b1: np.array):
-    W = np.matmul(np.linalg.inv(0.5 * (b0 + b1)), (m1 - m0))
+def get_W_fisher(m0: np.array, m1: np.array, b0: np.array, b1: np.array):
+    return np.matmul(np.linalg.inv(0.5 * (b0 + b1)), (m1 - m0))
 
-    sigma_0_s = np.matmul(np.matmul(W.T, b0), W)[0, 0]
-    sigma_1_s = np.matmul(np.matmul(W.T, b1), W)[0, 0]
 
-    w_n = - np.matmul(
+def get_sigma(W: np.array, b: np.array):
+    return np.matmul(np.matmul(W.T, b), W)[0, 0]
+
+
+def get_w_n(m0: np.array, m1: np.array, b0: np.array, b1: np.array, W: np.array):
+    sigma_0_s = get_sigma(W, b0)
+    sigma_1_s = get_sigma(W, b1)
+    return (- np.matmul(
         (np.matmul((m1 - m0).T, np.linalg.inv(0.5 * (b1 + b0)))),
-        (sigma_1_s * m0 + sigma_0_s * m1)) / (sigma_1_s + sigma_0_s)
+        (sigma_1_s * m0 + sigma_0_s * m1)) / (sigma_1_s + sigma_0_s))[0, 0]
 
-    return np.matmul(W.T, x) + w_n[0, 0]
+
+def fisher_discriminant(x: np.array, m0: np.array, m1: np.array, b0: np.array, b1: np.array):
+    W = get_W_fisher(m0, m1, b0, b1)
+    w_n = get_w_n(m0, m1, b0, b1, W)
+
+    return np.matmul(W.T, x) + w_n
 
 
 def fisher_classificator(x: np.array, m0: np.array, m1: np.array, b0: np.array, b1: np.array):
@@ -33,20 +45,32 @@ def fisher_classificator(x: np.array, m0: np.array, m1: np.array, b0: np.array, 
     return 1 if d > 0 else 0
 
 
-def get_z(dataset0: np.array, dataset1: np.array, k: int) -> tuple:
-    z0 = []
-    z1 = []
-    for i in range(k // 2):
-        z0.append(-dataset0[:, :, i])
-        z0[i] = np.append(z0[i], np.full((1, 1), -1), axis=0)
-        z1.append(dataset1[:, :, i])
-        z1[i] = np.append(z1[i], np.full((1, 1), 1), axis=0)
+def linear_border(y: np.array, W: np.array):
+    a = W[0]
+    b = W[1]
+    c = W[2]
 
-    return np.array(z0), np.array(z1)
+    if a == 0.:
+        return 0
+    else:
+        return (-b * y - c) / a
+
+
+def get_z(x: np.array, value):
+    return np.append(x, np.array([[value]]), axis=0)
 
 
 def get_U(dataset0: np.array, dataset1: np.array, k: int) -> np.array:
-    z0, z1 = get_z(dataset0, dataset1, k)
+    z0 = []
+    z1 = []
+
+    for i in range(k // 2):
+        z0.append(-dataset0[:, :, i])
+        z0[i] = get_z(z0[i], -1)
+
+        z1.append(dataset1[:, :, i])
+        z1[i] = get_z(z1[i], 1)
+
     return np.concatenate((z0, z1))
 
 
@@ -54,14 +78,14 @@ def get_Gamma(U: np.array) -> np.array:
     return np.ones((U.shape[0], 1, 1))
 
 
-def get_W(U: np.array, Gamma: np.array) -> np.array:
+def get_W_mse(U: np.array, Gamma: np.array) -> np.array:
     return np.matmul(
         np.matmul(np.linalg.inv(np.matmul(U[:, :, 0].T, U[:, :, 0])), U[:, :, 0].T),
         Gamma[:, :, 0])
 
 
 def mse_discriminant(x: np.array, W: np.array) -> np.array:
-    z = np.append(x, np.array([[1]]), axis=0)
+    z = get_z(x, 1)
     return np.matmul(W.T, z)
 
 
@@ -84,13 +108,32 @@ def main():
     dataset10 = np.load("../lab_1_dataset_generation/task_2_dataset_1.npy")
     dataset11 = np.load("../lab_1_dataset_generation/task_2_dataset_2.npy")
 
-    U = get_U(dataset00, dataset01, 40)
-    G = get_Gamma(U)
-    W = get_W(U, G)
-    for i in range(200):
-        if fisher_classificator(dataset00[:, :, i], m0, m1, b, b) - mse_classificator(dataset00[:, :, i], W) != 0:
-            print(f"Mismatch: {i}")
+    y = np.arange(-4, 4, 0.1)
+    W_fisher = get_W_fisher(m0, m1, b0, b1)
+    w_n = get_w_n(m0, m1, b0, b1, W_fisher)
+    W_fisher = np.append(W_fisher, np.array([[w_n]]), axis=0)
+    fisher_border_x = linear_border(y, W_fisher)
 
+    plt.figure()
+    plt.plot(dataset10[0, :, :], dataset10[1, :, :], color='red', marker='.')
+    plt.plot(dataset11[0, :, :], dataset11[1, :, :], color='green', marker='+')
+    plt.plot(fisher_border_x, y, color='black')
+    plt.xlim(left=-4, right=4)
+    plt.ylim(bottom=-4, top=4)
+
+    U = get_U(dataset10, dataset11, 200)
+    G = get_Gamma(U)
+    W_mse = get_W_mse(U, G)
+    mse_border_x = linear_border(y, W_mse)
+
+    plt.figure()
+    plt.plot(dataset10[0, :, :], dataset10[1, :, :], color='red', marker='.')
+    plt.plot(dataset11[0, :, :], dataset11[1, :, :], color='green', marker='+')
+    plt.plot(mse_border_x, y, color='black')
+    plt.xlim(left=-4, right=4)
+    plt.ylim(bottom=-4, top=4)
+
+    show()
 
 
 main()
