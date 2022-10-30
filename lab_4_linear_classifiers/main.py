fe51e9c1cd00221465f178af3056ea7cf675426e
meshcheryakov-a <meshcheryakov-a@internet.ru>
2022-10-31 02:16:02 +0400

    aci classificator wrong border

--- lab_4_linear_classifiers/main.py
+++ lab_4_linear_classifiers/main.py
@@ -17,8 +17,14 @@ def get_risk(aprior0: float, aprior1: float, p0: float, p1: float) -> float:
     return aprior0 * p0 + aprior1 * p1
 
 
+def expand(x: np.array, value):
+    return np.append(x, np.array([[value]]), axis=0)
+
+
 def get_W_fisher(m0: np.array, m1: np.array, b0: np.array, b1: np.array):
-    return np.matmul(np.linalg.inv(0.5 * (b0 + b1)), (m1 - m0))
+    W_fisher = np.matmul(np.linalg.inv(0.5 * (b0 + b1)), (m1 - m0))
+    w_n = get_w_n(m0, m1, b0, b1, W_fisher)
+    return expand(W_fisher, w_n)
 
 
 def get_sigma(W: np.array, b: np.array):
@@ -33,45 +39,29 @@ def get_w_n(m0: np.array, m1: np.array, b0: np.array, b1: np.array, W: np.array)
         (sigma_1_s * m0 + sigma_0_s * m1)) / (sigma_1_s + sigma_0_s))[0, 0]
 
 
-def fisher_discriminant(x: np.array, m0: np.array, m1: np.array, b0: np.array, b1: np.array):
-    W = get_W_fisher(m0, m1, b0, b1)
-    w_n = get_w_n(m0, m1, b0, b1, W)
-
-    return np.matmul(W.T, x) + w_n
-
-
-def fisher_classificator(x: np.array, m0: np.array, m1: np.array, b0: np.array, b1: np.array):
-    d = fisher_discriminant(x, m0, m1, b0, b1)
-    return 1 if d > 0 else 0
-
-
-def linear_border(y: np.array, W: np.array):
-    a = W[0]
-    b = W[1]
-    c = W[2]
-
-    if a == 0.:
-        return 0
-    else:
-        return (-b * y - c) / a
-
-
-def get_z(x: np.array, value):
-    return np.append(x, np.array([[value]]), axis=0)
+# def fisher_discriminant(x: np.array, m0: np.array, m1: np.array, b0: np.array, b1: np.array):
+#     W = get_W_fisher(m0, m1, b0, b1)
+#     w_n = get_w_n(m0, m1, b0, b1, W)
+#
+#     return np.matmul(W.T, x) + w_n
+#
+#
+# def fisher_classificator(x: np.array, m0: np.array, m1: np.array, b0: np.array, b1: np.array):
+#     d = fisher_discriminant(x, m0, m1, b0, b1)
+#     return 1 if d > 0 else 0
 
 
 def get_U(dataset0: np.array, dataset1: np.array, k: int) -> np.array:
-    z0 = []
-    z1 = []
+    U = []
 
     for i in range(k // 2):
-        z0.append(-dataset0[:, :, i])
-        z0[i] = get_z(z0[i], -1)
+        U.append(-dataset0[:, :, i])
+        U[i * 2] = expand(U[i * 2], -1)
 
-        z1.append(dataset1[:, :, i])
-        z1[i] = get_z(z1[i], 1)
+        U.append(dataset1[:, :, i])
+        U[i * 2 + 1] = expand(U[i * 2 + 1], 1)
 
-    return np.concatenate((z0, z1))
+    return np.array(U)
 
 
 def get_Gamma(U: np.array) -> np.array:
@@ -84,14 +74,54 @@ def get_W_mse(U: np.array, Gamma: np.array) -> np.array:
         Gamma[:, :, 0])
 
 
-def mse_discriminant(x: np.array, W: np.array) -> np.array:
-    z = get_z(x, 1)
+# def mse_discriminant(x: np.array, W: np.array) -> np.array:
+#     z = get_z(x, 1)
+#     return np.matmul(W.T, z)
+#
+#
+# def mse_classificator(x: np.array, W: np.array):
+#     d = mse_discriminant(x, W)
+#     return 1 if d > 0 else 0
+
+
+def get_alpha(k: int, beta: float):
+    return 1 / (k ** beta)
+
+
+def r(x: np.array):
+    return x[-1][0]
+
+
+# алгоритм корректирующих приращений
+def get_W_aci(W: np.array, U: np.array, k: int, beta: float):
+    W_prev = W_next = W
+
+    for i in range(1, k + 1):
+        x_i = U[i - 1, :, :]
+        alpha_i = get_alpha(i, beta)
+        W_next = W_prev + alpha_i * x_i * np.sign(r(x_i) - np.matmul(W_prev.T, x_i))
+        W_prev = W_next
+
+    return W_next
+
+
+def linear_discriminant(z: np.array, W: np.array) -> np.array:
     return np.matmul(W.T, z)
 
 
-def mse_classificator(x: np.array, W: np.array):
-    d = mse_discriminant(x, W)
-    return 1 if d > 0 else 0
+def linear_classificator(z: np.array, W: np.array):
+    return 1 if linear_discriminant(z, W) > 0 else 0
+
+
+def linear_border(y: np.array, W: np.array):
+    a = W[0]
+    b = W[1]
+    c = W[2]
+
+    if a == 0.:
+        return 0
+    else:
+        return (-b * y - c) / a
 
 
 def main():
@@ -110,8 +140,7 @@ def main():
 
     y = np.arange(-4, 4, 0.1)
     W_fisher = get_W_fisher(m0, m1, b0, b1)
-    w_n = get_w_n(m0, m1, b0, b1, W_fisher)
-    W_fisher = np.append(W_fisher, np.array([[w_n]]), axis=0)
+    print(f"Fisher : y = {-W_fisher[0] / W_fisher[1]} * x + {-W_fisher[2] / W_fisher[1]}")
     fisher_border_x = linear_border(y, W_fisher)
 
     plt.figure()
@@ -121,9 +150,11 @@ def main():
     plt.xlim(left=-4, right=4)
     plt.ylim(bottom=-4, top=4)
 
-    U = get_U(dataset10, dataset11, 200)
+    k = 400
+    U = get_U(dataset10, dataset11, k)
     G = get_Gamma(U)
     W_mse = get_W_mse(U, G)
+    print(f"mse : y = {-W_mse[0] / W_mse[1]} * x + {-W_mse[2] / W_mse[1]}")
     mse_border_x = linear_border(y, W_mse)
 
     plt.figure()
@@ -132,6 +163,18 @@ def main():
     plt.plot(mse_border_x, y, color='black')
     plt.xlim(left=-4, right=4)
     plt.ylim(bottom=-4, top=4)
+
+    beta = 0.7
+    W_aci = get_W_aci(np.full((3, 1), 1), U, k, beta)
+    print(f"aci : y = {-W_aci[0] / W_aci[1]} * x + {-W_aci[2] / W_aci[1]}")
+    aci_border_x = linear_border(y, W_aci)
+
+    plt.figure()
+    plt.plot(dataset10[0, :, :], dataset10[1, :, :], color='red', marker='.')
+    plt.plot(dataset11[0, :, :], dataset11[1, :, :], color='green', marker='+')
+    plt.plot(aci_border_x, y, color='black')
+    # plt.xlim(left=-4, right=4)
+    # plt.ylim(bottom=-4, top=4)
 
     show()
 
