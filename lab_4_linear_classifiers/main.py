8801af1059ad6a307c2234b9e8cf47ec2d3dee4f
meshcheryakov-a <meshcheryakov-a@internet.ru>
2022-10-31 20:12:19 +0400

    fixed aci border min mse ready

--- lab_4_linear_classifiers/main.py
+++ lab_4_linear_classifiers/main.py
@@ -51,7 +51,7 @@ def get_w_n(m0: np.array, m1: np.array, b0: np.array, b1: np.array, W: np.array)
 #     return 1 if d > 0 else 0
 
 
-def get_U(dataset0: np.array, dataset1: np.array, k: int) -> np.array:
+def get_U_mse(dataset0: np.array, dataset1: np.array, k: int) -> np.array:
     U = []
 
     for i in range(k // 2):
@@ -84,6 +84,23 @@ def get_W_mse(U: np.array, Gamma: np.array) -> np.array:
 #     return 1 if d > 0 else 0
 
 
+def get_U_robbins_monro(dataset0: np.array, dataset1: np.array, k: int):
+    U = []
+
+    for i in range(k // 2):
+        U.append(dataset0[:, :, i])
+        U[i * 2] = expand(U[i * 2], 1)
+        # метка класса для функции r
+        U[i * 2] = expand(U[i * 2], -1)
+
+        U.append(dataset1[:, :, i])
+        U[i * 2 + 1] = expand(U[i * 2 + 1], 1)
+        # метка класса для функции r
+        U[i * 2 + 1] = expand(U[i * 2 + 1], 1)
+
+    return np.array(U)
+
+
 def get_alpha(k: int, beta: float):
     return 1 / (k ** beta)
 
@@ -99,7 +116,20 @@ def get_W_aci(W: np.array, U: np.array, k: int, beta: float):
     for i in range(1, k + 1):
         x_i = U[i - 1, :, :]
         alpha_i = get_alpha(i, beta)
-        W_next = W_prev + alpha_i * x_i * np.sign(r(x_i) - np.matmul(W_prev.T, x_i))
+        W_next = W_prev + alpha_i * x_i[:-1] * np.sign(r(x_i) - np.matmul(W_prev.T, x_i[:-1]))
+        W_prev = W_next
+
+    return W_next
+
+
+# алгоритм наименьшей СКО
+def get_W_min_mse(W: np.array, U: np.array, k: int, beta: float):
+    W_prev = W_next = W
+
+    for i in range(1, k + 1):
+        x_i = U[i - 1, :, :]
+        alpha_i = get_alpha(i, beta)
+        W_next = W_prev + alpha_i * x_i[:-1] * (r(x_i) - np.matmul(W_prev.T, x_i[:-1]))
         W_prev = W_next
 
     return W_next
@@ -144,20 +174,22 @@ def main():
     fisher_border_x = linear_border(y, W_fisher)
 
     plt.figure()
+    plt.title("Fisher border")
     plt.plot(dataset10[0, :, :], dataset10[1, :, :], color='red', marker='.')
     plt.plot(dataset11[0, :, :], dataset11[1, :, :], color='green', marker='+')
     plt.plot(fisher_border_x, y, color='black')
     plt.xlim(left=-4, right=4)
     plt.ylim(bottom=-4, top=4)
 
-    k = 400
-    U = get_U(dataset10, dataset11, k)
-    G = get_Gamma(U)
-    W_mse = get_W_mse(U, G)
+    k = 200
+    U_mse = get_U_mse(dataset10, dataset11, k)
+    G = get_Gamma(U_mse)
+    W_mse = get_W_mse(U_mse, G)
     print(f"mse : y = {-W_mse[0] / W_mse[1]} * x + {-W_mse[2] / W_mse[1]}")
     mse_border_x = linear_border(y, W_mse)
 
     plt.figure()
+    plt.title("mse border")
     plt.plot(dataset10[0, :, :], dataset10[1, :, :], color='red', marker='.')
     plt.plot(dataset11[0, :, :], dataset11[1, :, :], color='green', marker='+')
     plt.plot(mse_border_x, y, color='black')
@@ -165,16 +197,34 @@ def main():
     plt.ylim(bottom=-4, top=4)
 
     beta = 0.7
-    W_aci = get_W_aci(np.full((3, 1), 1), U, k, beta)
+    k = 100
+    U_robbins_monro = get_U_robbins_monro(dataset10, dataset11, k)
+    W_aci = get_W_aci(np.full((3, 1), 1), U_robbins_monro, k, beta)
     print(f"aci : y = {-W_aci[0] / W_aci[1]} * x + {-W_aci[2] / W_aci[1]}")
     aci_border_x = linear_border(y, W_aci)
 
     plt.figure()
+    plt.title("aci border")
     plt.plot(dataset10[0, :, :], dataset10[1, :, :], color='red', marker='.')
     plt.plot(dataset11[0, :, :], dataset11[1, :, :], color='green', marker='+')
     plt.plot(aci_border_x, y, color='black')
-    # plt.xlim(left=-4, right=4)
-    # plt.ylim(bottom=-4, top=4)
+    plt.xlim(left=-4, right=4)
+    plt.ylim(bottom=-4, top=4)
+
+    k = 200
+    U_robbins_monro = get_U_robbins_monro(dataset10, dataset11, k)
+    W_min_mse = get_W_min_mse(np.full((3, 1), 1), U_robbins_monro, k, beta)
+    print(f"min mse : y = {-W_min_mse[0] / W_min_mse[1]} * x + {-W_min_mse[2] / W_min_mse[1]}")
+    min_mse_border_x = linear_border(y, W_min_mse)
+
+    plt.figure()
+    plt.title("min mse border")
+    plt.plot(dataset10[0, :, :], dataset10[1, :, :], color='red', marker='.')
+    plt.plot(dataset11[0, :, :], dataset11[1, :, :], color='green', marker='+')
+    plt.plot(min_mse_border_x, y, color='black')
+    plt.xlim(left=-4, right=4)
+    plt.ylim(bottom=-4, top=4)
+
 
     show()
 
