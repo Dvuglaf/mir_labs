843ae83be329a4eeebc7b0d16abe3850084ca386
meshcheryakov-a <meshcheryakov-a@internet.ru>
2022-11-16 23:40:20 +0400

    task 4 done

--- lab_6_svm/main.py
+++ lab_6_svm/main.py
@@ -16,6 +16,38 @@ def get_P(dataset, N):
     return P
 
 
+def get_P_kernel(dataset, N, K, params):
+    P = np.ndarray(shape=(N, N))
+    for i in range(N):
+        for j in range(N):
+            P[i, j] = r(dataset[j, :, :]) * r(dataset[i, :, :]) * K(dataset[j, :-1, :], dataset[i, :-1, :], params)
+    return P
+
+
+def K_polynom(x, y, params):
+    d = params[0]
+    c = params[1]
+    return pow(np.matmul(x.T, y)[0, 0] + c, d)
+
+
+def K_radial(x, y, params):
+    gamma = params[0]
+    return np.exp(-gamma * np.sum(np.power((x - y), 2)))
+
+
+def K_sigmoid(x, y, params):
+    gamma = params[0]
+    c = params[1]
+    return np.tanh(gamma * np.matmul(x.T, y)[0, 0] + c)
+
+
+def get_discriminant_kernel(support_vectors, _lambda_r, x, K, params):
+    sum = 0
+    for j in range(support_vectors.shape[0]):
+        sum += _lambda_r[j] * K(support_vectors[j].reshape(2, 1), x, params)
+    return sum
+
+
 def get_A(dataset, N):
     A = np.zeros((N,))
     for j in range(N):
@@ -103,8 +135,8 @@ def task2(dataset1, dataset2):
 
     # находим весовые коэффициенты из выражения через двойственные коэффициенты
     # и пороговое значение через весовые коэффициенты и опорные вектора
-    W = np.matmul((_lambda * A).T, dataset[:, :-1, 0]).reshape(2, 1)
-    w_N = np.mean(A[_lambda > 1e-04] - np.matmul(W.T, support_vectors.T))
+    W = np.matmul((_lambda * A)[support_vectors_positions].T, support_vectors).reshape(2, 1)
+    w_N = np.mean(A[support_vectors_positions] - np.matmul(W.T, support_vectors.T))
     W = expand(W, w_N)
 
     # обучение модели SVC (kernel=linear)
@@ -194,8 +226,8 @@ def task3(dataset3, dataset4):
 
         # находим весовые коэффициенты из выражения через двойственные коэффициенты
         # и пороговое значение через весовые коэффициенты и опорные вектора
-        W = np.matmul((_lambda * A).T, dataset[:, :-1, 0]).reshape(2, 1)
-        w_N = np.mean(A[_lambda > 1e-04] - np.matmul(W.T, dataset[_lambda > 1e-04, :-1, 0].T))
+        W = np.matmul((_lambda * A)[support_vectors_positions].T, support_vectors).reshape(2, 1)
+        w_N = np.mean(A[support_vectors_positions] - np.matmul(W.T, support_vectors.T))
         W = expand(W, w_N)
 
         # обучение модели SVC
@@ -244,6 +276,97 @@ def task3(dataset3, dataset4):
 б) метод sklearn.svm.SVC.
 """
 def task4(dataset3, dataset4):
+    N = dataset3.shape[2] + dataset4.shape[2]
+
+    # подготовка обучающей выборки
+    dataset = get_train_dataset(dataset3, dataset4)
+
+    # параметры для решения задачи квадратичного программирования
+    # kernel = 'poly'
+    # K = K_polynom
+    # params = [3, 1]
+
+    # kernel = 'rbf'
+    # K = K_radial
+    # params = [1]
+    # gauss
+    # var = np.var(np.sqrt(np.power(dataset[:, 0, :], 2) + np.power(dataset[:, 0, :], 2)))
+    # params = [1 / (2 * var)]
+
+    kernel = 'sigmoid'
+    K = K_sigmoid
+    params = [1 / 14, -1]
+
+    P = get_P_kernel(dataset, N, K, params)
+    q = np.full((N, 1), -1, dtype=np.double)
+    G = np.concatenate((np.eye(N) * -1, np.eye(N)), axis=0)
+    A = get_A(dataset, N)
+    b = np.zeros(1)
+
+    for C in [0.1, 1, 10]:
+        h = np.concatenate((np.zeros((N,)), np.full((N,), C)), axis=0)
+
+        # получаем вектор двойственных коэффициентов
+        _lambda = solve_qp(P, q, G, h, A, b, solver='cvxopt')
+
+        # опорные вектора для метода solve_qp
+        support_vectors_positions = _lambda > 1e-04
+        support_vectors = dataset[support_vectors_positions, :-1, 0]
+        support_vectors_classes = dataset[support_vectors_positions, -1, 0]
+        red_support_vectors = support_vectors[support_vectors_classes == -1]
+        green_support_vectors = support_vectors[support_vectors_classes == 1]
+
+        # находим пороговое значение через ядро и опорные вектора
+        w_N = []
+        for j in range(support_vectors.shape[0]):
+            w_N.append(get_discriminant_kernel(support_vectors, (_lambda * A)[support_vectors_positions],
+                                             support_vectors[j].reshape(2, 1), K, params))
+        w_N = np.mean(A[support_vectors_positions] - np.array(w_N))
+
+        # обучение модели SVC
+        # svc_clf = SVC(C=C, kernel=kernel, degree=3, coef0=1) # poly
+        # svc_clf = SVC(C=C, kernel=kernel, gamma=1) # radial
+        # svc_clf = SVC(C=C, kernel=kernel, gamma=1 / (2 * var)) # radial gauss
+        svc_clf = SVC(C=C, kernel=kernel, coef0=-1, gamma=1/14) # sigmoid
+        svc_clf.fit(dataset[:, :-1, 0], dataset[:, -1, 0])
+
+        # опорные вектора для метода SVC
+        support_vectors_svc = svc_clf.support_vectors_
+        support_vectors_svc_indices = svc_clf.support_
+        support_vectors_svc_classes = dataset[support_vectors_svc_indices, -1, 0]
+        red_support_vectors_svc = support_vectors_svc[support_vectors_svc_classes == -1]
+        green_support_vectors_svc = support_vectors_svc[support_vectors_svc_classes == 1]
+
+        # строим разделяющую полосу и разделяющую гиперплоскость
+        y = np.linspace(-4, 4, N)
+        x = np.linspace(-4, 4, N)
+        # создаем координатную сетку
+        xx, yy = np.meshgrid(x, y)
+        # получаем множество векторов
+        xy = np.vstack((xx.ravel(), yy.ravel())).T
+
+        # получаем множество значений решающей функции для нашей сетки (solve_qp)
+        discriminant_func_values = []
+        for i in range(xy.shape[0]):
+            discriminant_func_values.append(get_discriminant_kernel(support_vectors,
+                                                                  (_lambda * A)[support_vectors_positions],
+                                                                  xy[i].reshape(2, 1), K, params)
+                                            + w_N)
+        discriminant_func_values = np.array(discriminant_func_values).reshape(xx.shape)
+
+        # получаем множество значений решающей функции для нашей сетки (SVC)
+        discriminant_func_values_svc = svc_clf.decision_function(xy).reshape(xx.shape)
+
+        # разделяющая полоса
+        plot(f"solve_qp (cvxopt) ({kernel}) C={C}", dataset3, dataset4, [], [], ['black', 'green', 'red'], ['', '', ''])
+        plt.contour(xx, yy, discriminant_func_values, levels=[-1, 0, 1], colors=['red', 'black', 'green'])
+        plt.scatter(red_support_vectors[:, 0], red_support_vectors[:, 1], color='red')
+        plt.scatter(green_support_vectors[:, 0], green_support_vectors[:, 1], color='green')
+
+        plot(f"SVC ({kernel}) C={C}", dataset3, dataset4, [], [], ['black', 'green', 'red'], ['', '', ''])
+        plt.contour(xx, yy, discriminant_func_values_svc, levels=[-1, 0, 1], colors=['red', 'black', 'green'])
+        plt.scatter(red_support_vectors_svc[:, 0], red_support_vectors_svc[:, 1], color='red')
+        plt.scatter(green_support_vectors_svc[:, 0], green_support_vectors_svc[:, 1], color='green')
 
     ...
 
@@ -256,7 +379,8 @@ def main():
     dataset4 = np.load("../lab_6_svm/dataset_4.npy")
 
     # task2(dataset1, dataset2)
-    task3(dataset3, dataset4)
+    # task3(dataset3, dataset4)
+    task4(dataset3, dataset4)
 
     show()
 
